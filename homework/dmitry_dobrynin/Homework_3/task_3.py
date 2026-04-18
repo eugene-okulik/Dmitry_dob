039559a60a20814cc7182675d669aaf04bc42839
Dmitry Dobrynin <dobrynin.trend@gmail.com>
2026-04-18 17:02:15 +0700

    homework_3_done

---
+++ homework/dmitry_dobrynin/Homework_3/task_3.py
@@ -0,0 +1,4 @@
+first_num = 2
+second_num = 8
+print(f'Среднее арифмитеческое: {(first_num + second_num) / 2} \n'
+      f'Среднее географическое: {(first_num * second_num) ** (1/2)}')
---
+++ homework/dmitry_dobrynin/Homework_3/task_4.py
@@ -0,0 +1,4 @@
+first_leg = 3
+second_leg = 4
+print(f'Гипотенуза равна: {(first_leg ** 2 + second_leg ** 2) ** (1/2):.0f} \n'
+      f'Площадь равна: {(1 / 2) * first_leg * second_leg:.0f}')
