0eb80d720a7c7c44a5b06a055185fe7467c1ae26
Dmitry Dobrynin <dobrynin.trend@gmail.com>
2026-04-18 19:43:18 +0700

    homework_4_fixed

--- homework/dmitry_dobrynin/Homework_4/full_tasks.py
+++ homework/dmitry_dobrynin/Homework_4/full_tasks.py
@@ -1,12 +1,12 @@
 my_dict = {"tuple": ("Sochi", "Omsk", "Moscow", "Novosibirsk", "Samara", True),
-           "list": ["PM", "Dev", "QA", "PO", "UI/UX", None], 
+           "list": ["PM", "Dev", "QA", "PO", "UI/UX", None],
            "dict": {"first_name": "Dmitry",
                     "middle_name": "Victorovich",
                     "last_name": "Dobrynin",
                     "city": "Krasnoyarsk",
                     "age": 27,
                     "salary": 123_567.89
-                    }, 
+                    },
            "set": {"Russia", "Germany", "Georgia", "China", "USA"}
            }
 
@@ -17,7 +17,7 @@ print(my_dict['tuple'][-1])
 # Задание 2
 
 my_dict['list'].append('Автоматизация')
-my_dict['list'].pop(1) # выбрал единицу, потому что второй элемент находится под индексом 1
+my_dict['list'].pop(1)  # выбрал единицу, потому что второй элемент находится под индексом 1
 
 # Задание 3
 
@@ -27,10 +27,9 @@ my_dict['dict'].pop('middle_name')
 # Задание 4
 
 my_dict['set'].add('Japan')
-my_dict['set'].pop() # удаляю случайный элемент из множества
-random_list = list(set([1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1])) # удаляю дубликаты из списка с помощью множества
+my_dict['set'].pop()  # удаляю случайный элемент из множества
+random_list = list(set([1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]))  # удаляю дубликаты из списка с помощью множества
 
-# Итог 
+# Итог
 
 print(my_dict)
-print('Опять забыл оставить в конце пустую строку :)')
