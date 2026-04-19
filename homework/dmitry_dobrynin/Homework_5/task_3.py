0a8edf1ebc26feceeeef48f499f2c7fd6f716c01
Dmitry Dobrynin <dobrynin.trend@gmail.com>
2026-04-19 18:39:10 +0700

    homework_5_minor_fix

--- homework/dmitry_dobrynin/Homework_5/task_3.py
+++ homework/dmitry_dobrynin/Homework_5/task_3.py
@@ -4,4 +4,8 @@ subjects = ['math', 'biology', 'geography']
 print(
     f'Students {students[0]}, {students[1]}, {students[2]} '
     f'study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}'
-    )
+)
+
+# либо
+
+print(f'Students {', '.join(students)} study these subjects: {', '.join(subjects)}')
