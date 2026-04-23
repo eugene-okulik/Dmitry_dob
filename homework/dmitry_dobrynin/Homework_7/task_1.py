6be7b0d0248344b4d10f200022f0edf1ac3d633c
Dmitry Dobrynin <dobrynin.trend@gmail.com>
2026-04-23 13:54:19 +0700

    homework_7_done

---
+++ homework/dmitry_dobrynin/Homework_7/task_1.py
@@ -0,0 +1,17 @@
+num_user = input("Угадайте цифру, введите целое цисло: ")
+main_num = 4
+while True:
+    if num_user.isdigit():
+        if int(num_user) == main_num:
+            print("Поздравляю! Вы угадали!")
+            break
+        else:
+            num_user = input("Попробуйте снова: ")
+    elif "-" in num_user and num_user.count("-") == 1 and num_user[1:].isdigit():
+        if int(num_user) == main_num:
+            print("Поздравляю! Вы угадали!")
+            break
+        else:
+            num_user = input("Попробуйте снова: ")
+    else:
+        num_user = input("Ввод некорректный, ожидается целое число. Попробуйте снова: ")
---
+++ homework/dmitry_dobrynin/Homework_7/task_3.py
@@ -0,0 +1,8 @@
+def calc(arg):
+    return int(arg.strip().split()[-1]) + 10
+
+
+print(calc("результат операции: 42"))
+print(calc("результат операции: 54"))
+print(calc("результат работы программы: 209"))
+print(calc("результат: 2"))
