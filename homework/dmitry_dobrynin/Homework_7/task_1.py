dae2e7072ece28562527b25a83a5aa753790fc70
Dmitry Dobrynin <dobrynin.trend@gmail.com>
2026-04-24 13:26:50 +0700

    homework_7_fix

--- homework/dmitry_dobrynin/Homework_7/task_1.py
+++ homework/dmitry_dobrynin/Homework_7/task_1.py
@@ -1,17 +1,39 @@
-num_user = input("Угадайте цифру, введите целое цисло: ")
+# num_user = input("Угадайте цифру, введите целое цисло: ")
+# main_num = 4
+# while True:
+#     if num_user.isdigit():
+#         if int(num_user) == main_num:
+#             print("Поздравляю! Вы угадали!")
+#             break
+#         else:
+#             num_user = input("Попробуйте снова: ")
+#     elif "-" in num_user and num_user.count("-") == 1 and num_user[1:].isdigit():
+#         if int(num_user) == main_num:
+#             print("Поздравляю! Вы угадали!")
+#             break
+#         else:
+#             num_user = input("Попробуйте снова: ")
+#     else:
+#         num_user = input("Ввод некорректный, ожидается целое число. Попробуйте снова: ")
+
+
+# Вторая реализация
+
+
 main_num = 4
 while True:
+    num_user = input("Угадайте цифру, введите целое цисло: ")
     if num_user.isdigit():
         if int(num_user) == main_num:
             print("Поздравляю! Вы угадали!")
             break
         else:
-            num_user = input("Попробуйте снова: ")
+            print("Попробуйте снова")
     elif "-" in num_user and num_user.count("-") == 1 and num_user[1:].isdigit():
         if int(num_user) == main_num:
             print("Поздравляю! Вы угадали!")
             break
         else:
-            num_user = input("Попробуйте снова: ")
+            print("Попробуйте снова")
     else:
-        num_user = input("Ввод некорректный, ожидается целое число. Попробуйте снова: ")
+        print("Ввод некорректный, ожидается целое число. Попробуйте снова")
--- homework/dmitry_dobrynin/Homework_7/task_2.py
+++ homework/dmitry_dobrynin/Homework_7/task_2.py
@@ -1,3 +1,11 @@
+# words = {"I": 3, "love": 5, "Python": 1, "!": 50}
+# for key, value in words.items():
+#     print(key * int(value))
+
+
+# вторая реализация
+
+
 words = {"I": 3, "love": 5, "Python": 1, "!": 50}
 for key, value in words.items():
-    print(key * int(value))
+    print(key * value)
