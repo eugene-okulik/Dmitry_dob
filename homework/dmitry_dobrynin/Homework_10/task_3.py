64ee2747496ec7f92b9ebd5958faf2070f7a7c5a
Dmitry Dobrynin <dobrynin.trend@gmail.com>
2026-05-02 18:06:49 +0700

    homework_10_fix

--- homework/dmitry_dobrynin/Homework_10/task_3.py
+++ homework/dmitry_dobrynin/Homework_10/task_3.py
@@ -1,5 +1,3 @@
-#  Первая реализация. Получилось вначале так решить, но я чуть условие поменял
-
 first = int(input("Введите первое число: "))
 second = int(input("Введите второе число: "))
 
@@ -15,59 +13,15 @@ def calc_operation(func):
             operation = "-"
         elif first < second:
             operation = "/"
-        func(first, second, operation)
-        print("Операция завершена")
+        result = func(first, second, operation)
+        if result is not None:
+            print(result)
 
     return wrapper
 
 
 @calc_operation
-def calc(first, second, operation):
-    if operation == "+":
-        print(first + second)
-    elif operation == "-":
-        print(first - second)
-    elif operation == "/":
-        print(first / second)
-    elif operation == "*":
-        print(first * second)
-    elif operation == "**":
-        print(first**second)
-    else:
-        print(
-            "Операция нам неизвестна, воспользуйтесь другим калькулятором."
-            " Если необходимо вычесть корень квадратный, то возведите в степень 0.5"
-        )
-
-
-calc(first, second, "+")
-
-
-# Вторая реализация
-
-first = int(input("Введите первое число: "))
-second = int(input("Введите второе число: "))
-
-
-def calc_operation(func):
-
-    def wrapper(first, second, operation):
-        if first < 0 or second < 0:
-            operation = "*"
-        elif first == second:
-            operation = "+"
-        elif first > second:
-            operation = "-"
-        elif first < second:
-            operation = "/"
-        print(func(first, second, operation))
-        print("Операция завершена")
-
-    return wrapper
-
-
-@calc_operation
-def calc(first, second, operation):
+def calc_second(first, second, operation):
     if operation == "+":
         return first + second
     elif operation == "-":
@@ -85,4 +39,26 @@ def calc(first, second, operation):
         )
 
 
-calc(first, second, ")")
+calc_second(first, second, "+")
+
+
+@calc_operation
+def calc_first(first, second, operation):
+    if operation == "+":
+        print(first + second)
+    elif operation == "-":
+        print(first - second)
+    elif operation == "/":
+        print(first / second)
+    elif operation == "*":
+        print(first * second)
+    elif operation == "**":
+        print(first**second)
+    else:
+        print(
+            "Операция нам неизвестна, воспользуйтесь другим калькулятором."
+            " Если необходимо вычесть корень квадратный, то возведите в степень 0.5"
+        )
+
+
+calc_first(first, second, "+")
