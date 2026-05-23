f5cede28fb8e3bd9539ad6659fcf1f37701bf76e
Dmitry Dobrynin <dobrynin.trend@gmail.com>
2026-05-23 17:56:09 +0700

    homework_13_fix

--- homework/dmitry_dobrynin/Homework_13/task_1.py
+++ homework/dmitry_dobrynin/Homework_13/task_1.py
@@ -21,15 +21,15 @@ for data_line in read_files():
     )
     if "на неделю позже" in data_line.lower():
         print(
-            f"Задание было «распечатать эту дату, но на неделю позже», результат: {date_data_line + datetime.timedelta(days=7)}"
+            f'Задание было «распечатать эту дату, но на неделю позже», результат: {date_data_line + datetime.timedelta(days=7)}'
         )
     elif "какой это будет день недели" in data_line.lower():
         print(
-            f"Задание было «распечатать какой это будет день недели», результат: {date_data_line.strftime("%A")}"
+            f'Задание было «распечатать какой это будет день недели», результат: {date_data_line.strftime("%A")}'
         )
     elif "сколько дней назад была эта дата" in data_line.lower():
         print(
-            f"Задание было «распечатать сколько дней назад была эта дата», результат: {(time_now - date_data_line).days}"
+            f'Задание было «распечатать сколько дней назад была эта дата», результат: {(time_now - date_data_line).days}'
         )
     else:
-        print("Команда мне такая пока непонятна")
+        print('Команда мне такая пока непонятна')
