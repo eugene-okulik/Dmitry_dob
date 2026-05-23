5ea0955951c6eca62420425427752bc7b10fe122
Dmitry Dobrynin <dobrynin.trend@gmail.com>
2026-05-23 18:00:45 +0700

    homework_13_fix_linter

--- homework/dmitry_dobrynin/Homework_13/task_1.py
+++ homework/dmitry_dobrynin/Homework_13/task_1.py
@@ -3,7 +3,7 @@ import datetime
 import pathlib
 
 # base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-base_path = pathlib.Path(__file__).parents[3]  #  нашел такой способ простраивания пути
+base_path = pathlib.Path(__file__).parents[3]  # нашел такой способ простраивания пути
 need_path = os.path.join(base_path, "homework/eugene_okulik/hw_13", "data.txt")
 
 
@@ -20,16 +20,14 @@ for data_line in read_files():
         " ".join(data_line.split()[1:3]), "%Y-%m-%d %H:%M:%S.%f"
     )
     if "на неделю позже" in data_line.lower():
-        print(
-            f'Задание было «распечатать эту дату, но на неделю позже», результат: {date_data_line + datetime.timedelta(days=7)}'
-        )
+        result = date_data_line + datetime.timedelta(days=7)
+        print(f'Задание было «распечатать эту дату, но на неделю позже», результат: {result}')
     elif "какой это будет день недели" in data_line.lower():
         print(
             f'Задание было «распечатать какой это будет день недели», результат: {date_data_line.strftime("%A")}'
         )
     elif "сколько дней назад была эта дата" in data_line.lower():
-        print(
-            f'Задание было «распечатать сколько дней назад была эта дата», результат: {(time_now - date_data_line).days}'
-        )
+        days_ago = (time_now - date_data_line).days
+        print(f'Задание было «распечатать сколько дней назад была эта дата», результат: {days_ago}')
     else:
         print('Команда мне такая пока непонятна')
